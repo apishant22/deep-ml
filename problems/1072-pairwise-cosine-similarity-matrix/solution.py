bfd6b5248b6eb386817a4c5f25eea7b63a504832
apishant22 <45246539+apishant22@users.noreply.github.com>
2026-09-16 20:28:56 +0000

    Update: Pairwise Cosine Similarity Matrix

--- problems/1072-pairwise-cosine-similarity-matrix/solution.py
+++ problems/1072-pairwise-cosine-similarity-matrix/solution.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def pairwise_cosine_similarity(X):
     # Your code here
-    norms = np.maximum(np.linalg.norm(X, axis=1, keepdims=True),1)
+    norms = np.maximum(np.linalg.norm(X, axis=1, keepdims=True),1e-6)
     x_norm = X/norms
 
     return x_norm @ x_norm.T
